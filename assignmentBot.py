95c9e952e8c2d63d367823f4dcdf669c66eb5815
Ali Yaghoubian <79134287+AliYqb@users.noreply.github.com>
2021-09-27 16:24:28 +0330

    Update assignmentBot.py

--- assignmentBot.py
+++ assignmentBot.py
@@ -1,39 +1,50 @@
 import random
 import telebot
 
-print('Aliii')
-
-def hadsAdad(message):
-    answer_number = random.randint(0, 1000)
-    bot.send_message(message.chat.id, 'یک عدد بین ۰ تا ۱۰۰۰ بگو')
-    flag = 0
-    while True:
-        @bot.message_handler(func=lambda message: True)
-        def check_game(message):
-            if int(message.text) > answer_number:
-                bot.send_message(message.chat.id, 'کمترش کن')
-            elif int(message.text) < answer_number:
-                bot.send_message(message.chat.id, 'بیشترش کن')
-            elif int(message.text) == answer_number:
-                bot.send_message(message.chat.id, 'ماشالااااا خودشه\nپیداش کردی')
-                flag = 1
-        if flag == 1:
-            break
-
 bot = telebot.TeleBot("2019327355:AAF9JCQPiBsOQKrEivcNfGlAoFk5nWKQ0jc")
-# bot = telebot.TeleBot('https://thawing-meadow-17131.herokuapp.com/' + "2019327355:AAF9JCQPiBsOQKrEivcNfGlAoFk5nWKQ0jc")
+markUp = telebot.types.ReplyKeyboardMarkup(row_width=3)
+button_1 = telebot.types.KeyboardButton('موسیقی')
+button_2 = telebot.types.KeyboardButton('ویدیو')
+button_3 = telebot.types.KeyboardButton('عکس')
 
-print('Aliii')
+markUp.add(button_1, button_2, button_3)
 
-@bot.message_handler(['Start', 'start', 'شروع'])
-def wellcome(message):
-    first_name = message.from_user.first_name
-    bot.reply_to(message, f'{first_name} خوش اومدی')
-    print('Aliii')
-
-@bot.message_handler(['Game', 'game', 'بازی'])
-def play_game(message):
-    bot.reply_to(message, 'بازی حدس عدد')
-    hadsAdad(message)
+@bot.message_handler(commands=['start', 'Start'])
+def send_welcome(message):
+    bot.reply_to(message, 'سلام دوست من. وقتت بخیر')
     
+@bot.message_handler(commands=['help', 'Help', 'komak'])
+def help(message):
+    bot.reply_to(message, 'این فقط یک تست باته. پنیک نکن')
+
+@bot.message_handler(commands=['فال'])
+def send_فال(message):
+    answers = ['به فنا خواهی رفت' ,'به دیدار معشوق خوهی رفت' ,'به سفر خواهی رفت' ,'پولدار میشوی' ,'بدبخت تر خواهی شد']
+    answer = random.choice(answers)
+    bot.reply_to(message, answer)
+    
+@bot.message_handler(commands=['download', 'Download', 'دانلود'])
+def download(message):
+    bot.reply_to(message, 'کدوم یکی رو لازم داری ؟', reply_markup=markUp)
+    
+    
+@bot.message_handler(func=lambda message: True)
+def simple_messages(message):
+    if message.text == 'سلام':
+        bot.reply_to(message, 'علیک سلام')
+    elif message.text == 'خوبی؟':
+        bot.reply_to(message, 'نه فقط تو خوبی')
+    elif message.text == 'چطوری جون دل؟':
+        bot.reply_to(message, 'الهی صد هزار مرتبه شکر')
+    elif message.text == 'بوس':
+        bot.reply_to(message, 'جووون')
+    elif message.text == 'دوست دارم':
+        bot.reply_to(message, 'قلب')
+    elif message.text == 'عکس':
+        photo = open('image/photo.png', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    else:    
+        # bot.reply_to(message, 'نمیفهمم چی میگی')
+        bot.send_message(message.chat.id, 'نمیفهمم چی میگی')
+
 bot.polling()
