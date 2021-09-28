467ca73a2be813fc31801cb8e613565187bca797
Ali Yaghoubian <79134287+AliYqb@users.noreply.github.com>
2021-09-28 10:51:35 +0330

    Update assignmentBot.py

--- assignmentBot.py
+++ assignmentBot.py
@@ -2,7 +2,6 @@ import random
 import telebot
 
 bot = telebot.TeleBot("2019327355:AAF9JCQPiBsOQKrEivcNfGlAoFk5nWKQ0jc")
-game_flag = 1
 game_answer_number = random.randint(0, 1000)
 
 @bot.message_handler(commands=['Start', 'start', 'شروع'])
@@ -14,24 +13,15 @@ def wellcome(message):
 def play_game(message):
     bot.reply_to(message, 'بازی حدس عدد')
     bot.send_message(message.chat.id, 'یک عدد بین ۰ تا ۱۰۰۰ بگو')
-    global game_flag
-    game_flag = 1
-    print('0')
-    
-if game_flag == 1:
-    print('1')
+
+while True:
     @bot.message_handler(func=lambda message: True)
     def check_game(message):
-        print('2')
-        while True:
-            if int(message.text) > game_answer_number:
-                bot.send_message(message.chat.id, 'کمترش کن')
-            elif int(message.text) < game_answer_number:
-                bot.send_message(message.chat.id, 'بیشترش کن')
-            elif int(message.text) == game_answer_number:
-                bot.send_message(message.chat.id, 'ماشالااااا خودشه\nپیداش کردی')
-                global game_flag
-                game_flag = 0
-                break
+        if int(message.text) > game_answer_number:
+            bot.send_message(message.chat.id, 'کمترش کن')
+        elif int(message.text) < game_answer_number:
+            bot.send_message(message.chat.id, 'بیشترش کن')
+        elif int(message.text) == game_answer_number:
+            bot.send_message(message.chat.id, 'ماشالااااا خودشه\nپیداش کردی')
                 
 bot.polling()
