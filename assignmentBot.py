23d18c9174ee8c26bd08a19a00f990442546beef
Ali Yaghoubian <79134287+AliYqb@users.noreply.github.com>
2021-09-28 11:02:45 +0330

    Update assignmentBot.py

--- assignmentBot.py
+++ assignmentBot.py
@@ -14,15 +14,16 @@ def play_game(message):
     bot.reply_to(message, 'بازی حدس عدد')
     bot.send_message(message.chat.id, 'یک عدد بین ۰ تا ۱۰۰۰ بگو')
 
-@bot.message_handler(func=lambda message: True)
-def check_game(message):
-    if int(message.text) > game_answer_number:
-        bot.send_message(message.chat.id, 'کمترش کن')
-        check_game(message)
-    elif int(message.text) < game_answer_number:
-        bot.send_message(message.chat.id, 'بیشترش کن')
-        check_game(message)
-    elif int(message.text) == game_answer_number:
-        bot.send_message(message.chat.id, 'ماشالااااا خودشه\nپیداش کردی')
+def loop_game():
+    @bot.message_handler(func=lambda message: True)
+    def check_game(message):
+        if int(message.text) > game_answer_number:
+            bot.send_message(message.chat.id, 'کمترش کن')
+            loop_game()
+        elif int(message.text) < game_answer_number:
+            bot.send_message(message.chat.id, 'بیشترش کن')
+            loop_game()
+        elif int(message.text) == game_answer_number:
+            bot.send_message(message.chat.id, 'ماشالااااا خودشه\nپیداش کردی')
                 
 bot.polling()
