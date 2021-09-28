37b3e133f10e32b599c8af4189ed7b2e463b38a5
Ali Yaghoubian <79134287+AliYqb@users.noreply.github.com>
2021-09-28 08:58:28 +0330

    Update assignmentBot.py

--- assignmentBot.py
+++ assignmentBot.py
@@ -1,53 +1,32 @@
 import random
-import os
-
 import telebot
 
-TOKEN = os.environ["TOKEN"]
-bot = telebot.TeleBot(TOKEN)
-markUp = telebot.types.ReplyKeyboardMarkup(row_width=3)
-button_1 = telebot.types.KeyboardButton('موسیقی')
-button_2 = telebot.types.KeyboardButton('ویدیو')
-button_3 = telebot.types.KeyboardButton('عکس')
+bot = telebot.TeleBot("2019327355:AAF9JCQPiBsOQKrEivcNfGlAoFk5nWKQ0jc")
+game_flag = 0
+game_answer_number = random.randint(0, 1000)
 
-markUp.add(button_1, button_2, button_3)
+@bot.message_handler(commands=['Start', 'start', 'شروع'])
+def wellcome(message):
+    first_name = message.from_user.first_name
+    bot.reply_to(message, f'{first_name} خوش اومدی')
 
-@bot.message_handler(commands=['start', 'Start'])
-def send_welcome(message):
-    bot.reply_to(message, 'سلام دوست من. وقتت بخیر')
+@bot.message_handler(commands=['Game', 'game', 'بازی'])
+def play_game(message):
+    bot.reply_to(message, 'بازی حدس عدد')
+    bot.send_message(message.chat.id, 'یک عدد بین ۰ تا ۱۰۰۰ بگو')
+    game_flag = 1
     
-@bot.message_handler(commands=['help', 'Help', 'komak'])
-def help(message):
-    bot.reply_to(message, 'این فقط یک تست باته. پنیک نکن')
-
-@bot.message_handler(commands=['فال'])
-def send_فال(message):
-    answers = ['به فنا خواهی رفت' ,'به دیدار معشوق خوهی رفت' ,'به سفر خواهی رفت' ,'پولدار میشوی' ,'بدبخت تر خواهی شد']
-    answer = random.choice(answers)
-    bot.reply_to(message, answer)
+if game_flag == 1:
+    @bot.message_handler(func=lambda message: True)
+    def check_game(message):
+        while True:
+            if int(message.text) > game_answer_number:
+                bot.send_message(message.chat.id, 'کمترش کن')
+            elif int(message.text) < game_answer_number:
+                bot.send_message(message.chat.id, 'بیشترش کن')
+            elif int(message.text) == game_answer_number:
+                bot.send_message(message.chat.id, 'ماشالااااا خودشه\nپیداش کردی')
+                break
+        game_flag = 0
     
-@bot.message_handler(commands=['download', 'Download', 'دانلود'])
-def download(message):
-    bot.reply_to(message, 'کدوم یکی رو لازم داری ؟', reply_markup=markUp)
-    
-    
-@bot.message_handler(func=lambda message: True)
-def simple_messages(message):
-    if message.text == 'سلام':
-        bot.reply_to(message, 'علیک سلام')
-    elif message.text == 'خوبی؟':
-        bot.reply_to(message, 'نه فقط تو خوبی')
-    elif message.text == 'چطوری جون دل؟':
-        bot.reply_to(message, 'الهی صد هزار مرتبه شکر')
-    elif message.text == 'بوس':
-        bot.reply_to(message, 'جووون')
-    elif message.text == 'دوست دارم':
-        bot.reply_to(message, 'قلب')
-    elif message.text == 'عکس':
-        photo = open('image/photo.png', 'rb')
-        bot.send_photo(message.chat.id, photo)
-    else:    
-        # bot.reply_to(message, 'نمیفهمم چی میگی')
-        bot.send_message(message.chat.id, 'نمیفهمم چی میگی')
-
 bot.polling()
