4dfa11b0dd899a43258df153c53f9a70162245c5
Ali Yaghoubian <79134287+AliYqb@users.noreply.github.com>
2021-09-28 11:12:09 +0330

    Update assignmentBot.py

--- assignmentBot.py
+++ assignmentBot.py
@@ -2,7 +2,6 @@ import random
 import telebot
 
 bot = telebot.TeleBot("2019327355:AAF9JCQPiBsOQKrEivcNfGlAoFk5nWKQ0jc")
-game_answer_number = random.randint(0, 1000)
 
 @bot.message_handler(commands=['Start', 'start', 'شروع'])
 def wellcome(message):
@@ -13,18 +12,18 @@ def wellcome(message):
 def play_game(message):
     bot.reply_to(message, 'بازی حدس عدد')
     bot.send_message(message.chat.id, 'یک عدد بین ۰ تا ۱۰۰۰ بگو')
-    loop_game()
+    loop_game(random.randint(0, 1000))
 
-def loop_game():
+def loop_game(answer_number):
     @bot.message_handler(func=lambda message: True)
     def check_game(message):
-        if int(message.text) > game_answer_number:
+        if int(message.text) > answer_number:
             bot.send_message(message.chat.id, 'کمترش کن')
             loop_game()
-        elif int(message.text) < game_answer_number:
+        elif int(message.text) < answer_number:
             bot.send_message(message.chat.id, 'بیشترش کن')
             loop_game()
-        elif int(message.text) == game_answer_number:
+        elif int(message.text) == answer_number:
             bot.send_message(message.chat.id, 'ماشالااااا خودشه\nپیداش کردی')
                 
 bot.polling()
